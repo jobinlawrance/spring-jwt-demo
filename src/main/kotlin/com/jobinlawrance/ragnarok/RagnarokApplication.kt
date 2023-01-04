4de70485363476136d20b50166694ea2e4afade9
Jobin Lawrance <jobinlawrance@gmail.com>
2023-01-04 15:54:49 +0530

    Init Commit

---
+++ src/main/kotlin/com/jobinlawrance/ragnarok/RagnarokApplication.kt
@@ -0,0 +1,11 @@
+package com.jobinlawrance.ragnarok
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RagnarokApplication
+
+fun main(args: Array<String>) {
+	runApplication<RagnarokApplication>(*args)
+}
---
+++ src/test/kotlin/com/jobinlawrance/ragnarok/RagnarokApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.jobinlawrance.ragnarok
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RagnarokApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
